feb6add70284e977576696da970f6b3d16641305
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-04-07 07:25:02 +0700

    add featured slide

--- app/src/main/java/com/sketchproject/infogue/Featured.java
+++ app/src/main/java/com/sketchproject/infogue/Featured.java
@@ -1,38 +1,20 @@
 package com.sketchproject.infogue;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.widget.TextView;
+import android.widget.ImageView;
 
 public class Featured extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
 
     @Override
@@ -40,9 +22,6 @@ public class Featured extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_featured);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -50,59 +29,66 @@ public class Featured extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onPageSelected(int position) {
+                changePage(position);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int position) {
             }
         });
 
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
+    /**
+     * @param position index position of pager view
+     */
+    private void changePage(int position){
+        ImageView view;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_featured, menu);
-        return true;
-    }
+        view = (ImageView) findViewById(R.id.page1);
+        view.setImageResource(R.drawable.circle_featured_normal);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        view = (ImageView) findViewById(R.id.page2);
+        view.setImageResource(R.drawable.circle_featured_normal);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        view = (ImageView) findViewById(R.id.page3);
+        view.setImageResource(R.drawable.circle_featured_normal);
+
+        switch(position){
+            case 0:
+                view = (ImageView) findViewById(R.id.page1);
+                view.setImageResource(R.drawable.circle_featured_active);
+                break;
+            case 1:
+                view = (ImageView) findViewById(R.id.page2);
+                view.setImageResource(R.drawable.circle_featured_active);
+                break;
+            case 2:
+                view = (ImageView) findViewById(R.id.page3);
+                view.setImageResource(R.drawable.circle_featured_active);
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
+
         private static final String ARG_SECTION_NUMBER = "section_number";
 
         public PlaceholderFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
@@ -112,11 +98,27 @@ public class Featured extends AppCompatActivity {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_featured, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+
+            ImageView featuredBackground = (ImageView) rootView.findViewById(R.id.featured_background);
+            ImageView featuredContent = (ImageView) rootView.findViewById(R.id.featured_content);
+
+            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
+                case 1:
+                    featuredBackground.setImageDrawable(getResources().getDrawable(R.drawable.bg_featured_1));
+                    featuredContent.setImageDrawable(getResources().getDrawable(R.drawable.img_featured_1));
+                    break;
+                case 2:
+                    featuredBackground.setImageDrawable(getResources().getDrawable(R.drawable.bg_featured_2));
+                    featuredContent.setImageDrawable(getResources().getDrawable(R.drawable.img_featured_2));
+                    break;
+                case 3:
+                    featuredBackground.setImageDrawable(getResources().getDrawable(R.drawable.bg_featured_3));
+                    featuredContent.setImageDrawable(getResources().getDrawable(R.drawable.img_featured_3));
+                    break;
+            }
+
             return rootView;
         }
     }
@@ -142,19 +144,6 @@ public class Featured extends AppCompatActivity {
         public int getCount() {
             // Show 3 total pages.
             return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
         }
     }
 }
--- app/src/main/java/com/sketchproject/infogue/Splash.java
+++ app/src/main/java/com/sketchproject/infogue/Splash.java
@@ -1,16 +1,28 @@
 package com.sketchproject.infogue;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends Activity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash);
+
+        final int SPLASH_TIME_OUT = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent featuredScreen = new Intent(Splash.this, Featured.class);
+                featuredScreen.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(featuredScreen);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
 }
