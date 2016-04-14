ad11709bd7a823e2d3625eada2377396c373a197
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-04-15 01:56:55 +0700

    add comment activity

---
+++ app/src/main/java/com/sketchproject/infogue/activities/CommentActivity.java
@@ -0,0 +1,107 @@
+package com.sketchproject.infogue.activities;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.sketchproject.infogue.R;
+import com.sketchproject.infogue.models.Article;
+
+public class CommentActivity extends AppCompatActivity {
+
+    private ProgressDialog progress;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        progress = new ProgressDialog(this);
+        progress.setMessage("Loading Article Data");
+        progress.setIndeterminate(true);
+        progress.show();
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String htmlComments = getHtmlComment(extras.getString(Article.ARTICLE_SLUG), "info-gue");
+
+            WebView webDisqus = (WebView) findViewById(R.id.disqus_comment);
+            // set up disqus
+            WebSettings webSettings = webDisqus.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webSettings.setBuiltInZoomControls(false);
+            webSettings.setDisplayZoomControls(false);
+            webDisqus.requestFocusFromTouch();
+            webDisqus.setWebViewClient(new WebViewClient(){
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                    progress.dismiss();
+                }
+            });
+            webDisqus.setWebChromeClient(new WebChromeClient(){
+                @Override
+                public void onProgressChanged(WebView view, int newProgress) {
+                    super.onProgressChanged(view, newProgress);
+                    if(newProgress == 100){
+                        progress.dismiss();
+                    }
+                }
+            });
+            webDisqus.loadData(htmlComments, "text/html", null);
+        } else {
+            Toast.makeText(getBaseContext(), "Invalid comment data", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    public String getHtmlComment(String idPost, String shortName) {
+
+        return "<div id='disqus_thread'></div>"
+                + "<script type='text/javascript'>"
+                + "var disqus_identifier = '"
+                + idPost
+                + "';"
+                + "var disqus_shortname = '"
+                + shortName
+                + "';"
+                + " (function() { var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;"
+                + "dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';"
+                + "(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq); })();"
+                + "</script>";
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.info, menu);
+        return true;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
